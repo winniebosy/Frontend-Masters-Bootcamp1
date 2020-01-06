711191bd835a17d9c5b1eb267ab835e93f0bcd62
winniebosy <winniebosy@gmail.com>
2020-01-06 17:42:02 +0300

    getting all the navbar-burger elements

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 //event listener to load event using the DOMContentLoaded
 document.addEventListener('DOMContentLoaded', () => {
     
-
+//getting all the navbar-burger elements
+const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
 
 
 });
