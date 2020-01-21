d3bd01ff7304a04a1a898d9f3010505eb5f6702a
winniebosy <winniebosy@gmail.com>
2020-01-21 11:49:58 +0300

    use of fucntions and tempelate literals

--- javascriptlessons/main3.js
+++ javascriptlessons/main3.js
@@ -8,3 +8,14 @@ function addTwo(number){
 let myString = "The answer is : ";
 const finalAnswer = addTwo(9);
 console.log(`${myString}`, finalAnswer);
+
+//example 2 on functions
+
+function greet(greeting, firstName, lastName, townShip) {
+    return `${greeting}! ${firstName} ${lastName} from  ${townShip}`;
+}
+
+console.log (greet('Salutations', 'Winnie', 'Bosibori', 'Nyahururu'));
+console.log (greet('Salutations', 'Eddie', 'Matara', 'Kisii'));
+console.log (greet('Salutations', 'Ethan', 'Leshan', 'Narok'));
+console.log (greet('Salutations', 'Cynthia', 'Ateku', 'Kitale'));
