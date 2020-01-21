b49902ea907791c142bf250a545ec2cdc466380d
winniebosy <winniebosy@gmail.com>
2020-01-21 11:28:15 +0300

    function adding two number and tempelate literal

--- javascriptlessons/main3.js
+++ javascriptlessons/main3.js
@@ -1,1 +1,10 @@
 'use strict'
+
+
+//function on adding two numbers
+function addTwo(number){
+    return number + 2;
+}
+let myString = "The answer is : ";
+const finalAnswer = addTwo(9);
+console.log(`${myString}`, finalAnswer);
